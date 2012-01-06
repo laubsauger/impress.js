34eb282c487fb7b329972e92694918b46548213f
laubsauger <laubsauger@gmail.com>
2012-01-06 16:00:14 +0100

    cleaning

--- js/impress.studio.js
+++ js/impress.studio.js
@@ -1,3 +1,17 @@
+/**
+ * impress.studio.js
+ *
+ * impress.studio.js attempts to provide a rich user interface to create and edit impress.js presentations. 
+ *
+ *
+ * impress.js is a presentation tool based on the power of CSS3 transforms and transitions
+ * in modern browsers and inspired by the idea behind prezi.com.
+ * 	
+ * MIT Licensed.
+ *
+ * Copyright 2011 Bartek Szopka (@bartaz)
+ */
+ 
 jQuery(document).ready(function() {
 	jQuery(document).on("click", ".step", function() {
 		var el = jQuery(this);
