dfbfa4b20ff44bf5c7e06ecb9f762c2b53ea7372
it12 <f.hinze@eugster.ch>
2012-01-06 14:48:33 +0100

    cleaning

--- js/impress.studio.js
+++ js/impress.studio.js
@@ -1,12 +1,7 @@
 jQuery(document).ready(function() {
 	jQuery(document).on("click", ".step", function() {
 		var el = jQuery(this);
+		jQuery('.edit').removeClass('edit');
 		el.addClass('edit');
-		
-		jQuery(document).on('focusout', el, function() {
-			el.removeClass('edit');
-		});
 	});
-	
-	
 });
