203a3586fdee5f7cf8feb90bbe96d82d806470b8
laubsauger <laubsauger@gmail.com>
2012-01-12 00:59:39 +0100

    rougly integrated jushs editor into edit-content tool

--- js/impress.studio.js
+++ js/impress.studio.js
@@ -15,7 +15,7 @@ function createMenu() {
 		livelog_div = jQuery('.livelog'),
 		stepIdWasActive;
 		
-	function displaySubmenu(oMenuitem, osubmenu, position) {
+	function displaySubmenu (oMenuitem, osubmenu, position) {
 		jQuery(osubmenu).each(function(index, domEle){
 			var sub_id 			= parseInt(domEle.getAttribute('data-submenu-id'),10),
 				iPaddingBottom 	= 45,
@@ -36,7 +36,7 @@ function createMenu() {
 		//@todo: keep (displayed) log short or we will get hundreds of elements in a long session
 	}
 	
-	function openWidget(step, operation) {
+	function openWidget (step, operation) {
 		var stepData, 
 			stepDim, 
 			widget = jQuery('.widget.' + operation),
@@ -55,6 +55,10 @@ function createMenu() {
 		} else if (operation === 'save') {
 			save (step, widget, title);
 		}
+	}
+	
+	function onCloseWidgetRemoveGizmos (stepOverlay, widget) {
+		$(stepOverlay).hide();
 	}
 	
 	function addNewStep (id, className, content) {
@@ -81,13 +85,17 @@ function createMenu() {
 		$('.widget.save textarea').text(cssFile + htmlStr + jsFile);
 	}
 	
-	function editPosition(step, widget, title) {
+	function editPosition (step, widget, title) {
 		var moveX, 
 			moveY, 
 			moveZ,
 			stepData = getStepData(step),
 			stepDim = getStepDim(step),
 			PosDataNew,
+			stepOverlay		= $('.step-edit-overlay'),
+			stepDim 		= getStepDim (step),
+			stepIdActive   	= $(step).attr('id'),
+			overlayVisible	= $('.step-edit-overlay:visible').length || 0,
 			options = {
 					buttons: {
 						"Refresh": function() {
@@ -106,11 +114,32 @@ function createMenu() {
 							//drawCanvas(document, window);
 						}
 					},
+					beforeClose: function() {onCloseWidgetRemoveGizmos (stepOverlay, widget)},
 					width: 215,
 					resizable: false,
 					title: title + jQuery(step).attr('id')
 				};
+			
+			//apply/remove overlay
+			if (overlayVisible === 0) {
+				jQuery(step).append(stepOverlay);
+				jQuery(stepOverlay).show();
+				resizeOverlay();
+			} else if (stepIdWasActive === stepIdActive) { 
+				jQuery(stepOverlay).hide();
+			} else if (stepIdWasActive !== stepIdActive) {
+				jQuery(step).append(stepOverlay);
+				jQuery(stepOverlay).show();
+				resizeOverlay();
+			}
 				
+				stepIdWasActive = stepIdActive;
+			
+			function resizeOverlay() {
+				$(stepOverlay).css({height: (stepDim.height) + "px", width: (stepDim.width) + "px", top: 0 + "px", left: 0 + "px"})
+				//@todo: respect scale / perspective to fit properly
+			}
+			
 			function refreshWidgetData () {
 				jQuery(widget).children().children('input').val(0);
 				stepDim = getStepDim(step);
@@ -165,13 +194,13 @@ function createMenu() {
 		return stepDimension;
 	}
 	
-	function rotateStep(step, rotateX, rotateY, rotateZ) {
+	function rotateStep (step, rotateX, rotateY, rotateZ) {
 		step.attr('data-rotate-x', parseInt(rotateX) || 0);
 		step.attr('data-rotate-y', parseInt(rotateY) || 0);
 		step.attr('data-rotate-z', parseInt(rotateZ) || 0);
 	}
 	
-	function setStepDataPos(step, moveDataX, moveDataY, moveDataZ) {
+	function setStepDataPos (step, moveDataX, moveDataY, moveDataZ) {
 		//get step posi
 		var pos = getStepData(step);
 			
@@ -180,46 +209,61 @@ function createMenu() {
 		step.attr('data-z', pos.z + ( parseInt(moveDataZ) || 0 ));
 	}
 	
+	
+/* jush's slide content editor */ 
+	function createEditor (step) {
+		// Replace the contents of the current slide with its own html to be edited
+		var ownHtml = $(step).html();
+		$(step).append('<textarea class="impress_slide_content">' +  ownHtml + '</textarea>');
+
+		// Disable click handle
+		$(step).off("click");
+		document.removeEventListener("keydown", document.filterKeys, false);
+
+		// Add button to save changes
+		$(step).append($('<button type="button">Save</button>').click({slide:this}, saveContent));
+	}
+
+	function saveContent (event) {
+		var slide = event.data.slide;
+		$(slide).click(createEditor);
+		var newContent = $(slide).children(".impress_slide_content")[0].value;
+		$(slide).empty();
+		$(slide).append(newContent);
+		// Re-enable impress.js key navigation
+		document.addEventListener("keydown", document.filterKeys, false);
+		// Avoid calling createEditor immediately by not propagating the event
+		return false;
+	}
+/* jush's slide content editor */ 	
+
+	
 	//right click step to select it for editing
 	jQuery('.step').bind('contextmenu', function(e) {
-		var step_selected 	= jQuery('.step-selected'), 
-			stepOverlay		= jQuery('.step-edit-overlay'),
+		var step_selected 	= jQuery('.step-selected'),
 			stepDim 		= getStepDim (this),
-			stepIdActive   	= $(this).attr('id'),
-			overlayVisible	= jQuery('.step-edit-overlay:visible').length || 0;
+			overlayVisible	= jQuery('.step-edit-overlay:visible').length || 0,
+			stepIdActive   	= $(this).attr('id');
 			
 		//highlight selected frame
 		jQuery(this).toggleClass('step-selected');
 		jQuery('.step-selected').not(this).removeClass('step-selected');
-				
+		
+		
 		//apply/remove overlay
 		if (overlayVisible === 0) {
-			jQuery(this).append(stepOverlay);
-			jQuery(stepOverlay).show();
-			resizeOverlay();
-			
-			//push some step data to log 
+			//push some step data to log
 			livelog (livelog_div, "<b>" + this.getAttribute('id') + " </b> selected");
-			livelog (livelog_div, "X " + stepDim.left + " Y " + stepDim.top + " | Dim " + stepDim.height + " x " + stepDim.width);
-		} else if (stepIdWasActive === stepIdActive) { 
-			jQuery(stepOverlay).hide();
+			livelog (livelog_div, "Y " + stepDim.top + " X " + stepDim.left + " | Dim " + stepDim.height + " x " + stepDim.width);
+		} else if (stepIdWasActive === stepIdActive) {
+			//do nothing - same step
 		} else if (stepIdWasActive !== stepIdActive) {
-			jQuery(this).append(stepOverlay);
-			jQuery(stepOverlay).show();
-			resizeOverlay();
-			
-			//push some step data to log 
+			//push some step data to log
 			livelog (livelog_div, "<b>" + this.getAttribute('id') + " </b> selected");
-			livelog (livelog_div, "X " + stepDim.left + " Y " + stepDim.top + " | Dim " + stepDim.height + " x " + stepDim.width);
+			livelog (livelog_div, "Y " + stepDim.top + " X " + stepDim.left + " | Dim " + stepDim.height + " x " + stepDim.width);
 		}
-		
+
 		stepIdWasActive = stepIdActive;
-		
-		function resizeOverlay() {
-			$(stepOverlay).css({height: (stepDim.height) + "px", width: (stepDim.width) + "px", top: 0 + "px", left: 0 + "px"})
-			//@todo: respect scale / perspective to fit properly
-		}
-		
 		//prevent contextmenu default action
 		e.preventDefault();
 		return false;
@@ -280,6 +324,16 @@ function createMenu() {
 
 				case "edit-content": 
 					livelog (livelog_div, "<i>submenu:edit-content</i>");
+					
+					var step = jQuery('.step-selected');
+					
+					if(step.length > 0) {
+						openWidget(step, submenuFunc);
+					}else {
+						livelog (livelog_div, "--> error: can't edit - no step selected!");
+					}
+					createEditor(step);
+					
 					break;
 				
 				case "edit-position":
